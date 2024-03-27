afccbae6b592eb88feee5cfd05745ec47cb94353
MoralesAdrian1 <135056297+MoralesAdrian1@users.noreply.github.com>
2024-03-27 15:07:01 -0600

    cambios que pidio alex

--- src/client/src/app/components/perfil-admin/perfil-admin.component.ts
+++ src/client/src/app/components/perfil-admin/perfil-admin.component.ts
@@ -4,100 +4,97 @@ import { PerfilModel } from 'src/app/models/perfilModel';
 import { PerfilAdminService } from 'src/app/services/perfil-admin.service';
 
 @Component({
-  selector: 'app-perfil-admin',
-  templateUrl: './perfil-admin.component.html',
-  styleUrls: ['./perfil-admin.component.css']
+    selector: 'app-perfil-admin',
+    templateUrl: './perfil-admin.component.html',
+    styleUrls: ['./perfil-admin.component.css']
 })
 export class PerfilAdminComponent implements OnInit {
-  perfil: PerfilModel[] = [];
-  perfilForm: FormGroup;
+    perfil: PerfilModel[] = [];
+    perfilForm: FormGroup;
 
-  constructor(private perfilService: PerfilAdminService, private fb: FormBuilder) {
-    this.perfilForm = this.fb.group({
-      _id: [''],
-      puesto: ["", Validators.required],
-      lenguajeProgramacion: [[], Validators.required], // Cambio para recibir un array
-      Sos: [[], Validators.required], // Cambio para recibir un array
-      yearsExperiencia: [0, Validators.required],
-      idiomas: [[], Validators.required], // Cambio para recibir un array
-      certificaciones: [[]], // Cambio para recibir un array vacío
-    });
-  }
+    lenguajesProgramacionDisponibles: string[] = ["JavaScript", "Java", "Python", "JSX", "Kotlin"];
+    sistemasOperativosDisponibles: string[] = ["Windows", "Linux", "MacOS"];
+    idiomasDisponibles: string[] = ["Inglés", "Español", "Francés"];
+    certificacionesDisponibles: string[] = ["Certificación 1", "Certificación 2", "Certificación 3"];
 
-  ngOnInit() {
-    this.cargarPerfil();
-  }
+    constructor(private perfilService: PerfilAdminService, private fb: FormBuilder) {
+        this.perfilForm = this.fb.group({
+            _id: [''],
+            puesto: ["", Validators.required],
+            lenguajeProgramacion: [[]],
+            Sos: [[]],
+            yearsExperiencia: [0, Validators.required],
+            idiomas: [[]],
+            certificaciones: [[]],
+        });
+    }
 
-  cargarPerfil() {
-    this.perfilService.getPerfil().subscribe(
-      data => {
-        this.perfil = data;
-      },
-      error => {
-        console.error('Error al cargar datos Personales:', error);
-      }
-    );
-  }
+    ngOnInit() {
+        this.cargarPerfil();
+    }
 
-  agregarPerfil() {
-    if (this.perfilForm.valid) {
-        // Dividir los valores de habilidades, Sos, idiomas y certificaciones por comas
-        const lenguajeProgramacion = this.perfilForm.value.lenguajeProgramacion.split(',');
-        const Sos = this.perfilForm.value.Sos.split(',');
-        const idiomas = this.perfilForm.value.idiomas.split(',');
-        const certificaciones = this.perfilForm.value.certificaciones.split(',');
-
-        // Construir un nuevo objeto con los datos actualizados
-        const nuevoPerfil = {
-            ...this.perfilForm.value,
-            lenguajeProgramacion,
-            Sos,
-            idiomas,
-            certificaciones
-        };
-
-        // Llamar al método del servicio para agregar el perfil
-        this.perfilService.addPerfil(nuevoPerfil).subscribe(
-            () => {
-                this.cargarPerfil();
-                this.perfilForm.reset();
+    cargarPerfil() {
+        this.perfilService.getPerfil().subscribe(
+            data => {
+                this.perfil = data;
             },
             error => {
-                console.error('Error al agregar perfil', error);
+                console.error('Error al cargar datos Personales:', error);
             }
         );
     }
-}
 
-  actualizarPerfil() {
-    if (this.perfilForm.valid) {
-      this.perfilService.updatePerfil(this.perfilForm.value).subscribe(
-        () => {
-          this.cargarPerfil();
-          this.perfilForm.reset();
-        },
-        error => {
-          console.error('Error al actualizar perfil:', error);
+    agregarPerfil() {
+        if (this.perfilForm.valid) {
+            const nuevoPerfil = {
+                ...this.perfilForm.value
+            };
+
+            this.perfilService.addPerfil(nuevoPerfil).subscribe(
+                () => {
+                    this.cargarPerfil();
+                    this.perfilForm.reset();
+                },
+                error => {
+                    console.error('Error al agregar perfil', error);
+                }
+            );
         }
-      );
     }
-  }
 
-  eliminarPerfil(id: string | undefined) {
-    if (id) {
-      this.perfilService.deletePerfil(id).subscribe(
-        data => {
-          console.log('Perfil eliminado:', data);
-          this.cargarPerfil();
-        },
-        error => {
-          console.error('Error al eliminar Perfil:', error);
+    actualizarPerfil() {
+        if (this.perfilForm.valid) {
+            const perfilActualizado = {
+                ...this.perfilForm.value
+            };
+
+            this.perfilService.updatePerfil(perfilActualizado).subscribe(
+                () => {
+                    this.cargarPerfil();
+                    this.perfilForm.reset();
+                },
+                error => {
+                    console.error('Error al actualizar perfil:', error);
+                }
+            );
         }
-      );
     }
-  }
 
-  editarPerfil(perfil: PerfilModel) {
-    this.perfilForm.patchValue(perfil);
-  }
+    eliminarPerfil(id: string | undefined) {
+        if (id) {
+            this.perfilService.deletePerfil(id).subscribe(
+                data => {
+                    console.log('Perfil eliminado:', data);
+                    this.cargarPerfil();
+                },
+                error => {
+                    console.error('Error al eliminar Perfil:', error);
+                }
+            );
+        }
+    }
+
+    editarPerfil(perfil: PerfilModel) {
+        this.perfilForm.patchValue(perfil);
+    }
 }
